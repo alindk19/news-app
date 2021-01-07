05c10f3276768508de75d9b45abf728764150407
Alind <alind.321@gmail.com>
2021-01-07 19:34:14 +0530

    error page redesigned

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Error = ({ error }) => {
-  return <div>Could not fetch news articles!!</div>;
+  return (
+    <div className="error center">
+      <h1>Error retrieving News.</h1>
+    </div>
+  );
 };
 
 export default Error;
